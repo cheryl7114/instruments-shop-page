af25e56ca8cc304f220e6d85e2484fdb3b9b0dcb
harrist03 <harristeh85@gmail.com>
2025-02-20 20:04:43 +0000

    navbar completed responsive not completed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,36 @@
 import React from "react";
+import { CiUser } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
 
 export default class Navbar extends React.Component {
   render() {
     return (
       <nav className="navbar">
-        <div className="logo">
-          <a href="../components/DisplayAllProducts.js">
-            <img src="/logo.png" alt="Logo" width="70" height="90"/>
-          </a>
+        <div className="most-left-side">
+          <div className="logo">
+            <a href="../components/DisplayAllProducts.js">
+              <img src="/logo.png" alt="Logo" width="50" height="60" />
+            </a>
+          </div>
+          <ul className="nav-links">
+            <li><a href="#">Guitar</a></li>
+            <li><a href="#">Piano</a></li>
+            <li><a href="#">Drums</a></li>
+            <li><a href="#">Violin</a></li>
+            <li><a href="#">Shop All</a></li>
+          </ul>
         </div>
-        <ul className="nav-links">
-          <li><a href="#">help</a></li>
-          <li><a href="#">orders and returns</a></li>
-        </ul>
+        <div className="most-right-side">
+          <div className="search-bar">
+            <CiSearch className="search-icon" />
+            <input type="text" placeholder="Search..." />
+          </div>
+
+          <CiUser className="user-icon" />
+          <CiShoppingCart className="cart-icon" />
+        </div>
       </nav>
-    );
+    )
   }
 }
