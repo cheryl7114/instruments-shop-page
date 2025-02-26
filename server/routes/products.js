21ea51a971b473f96a0149b8ab8e7fe795f397c5
harrist03 <harristeh85@gmail.com>
2025-02-26 00:30:54 +0000

    middleware functions added for users and products in routes/

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,116 +1,72 @@
 const router = require(`express`).Router()
-
 const productsModel = require(`../models/products`)
-
 const jwt = require('jsonwebtoken')
 const fs = require('fs')
 const JWT_PRIVATE_KEY = fs.readFileSync(process.env.JWT_PRIVATE_KEY_FILENAME, 'utf8')
 
+const verifyUsersJWTPassword = (req, res, next) => {
+    jwt.verify(req.headers.authorization, JWT_PRIVATE_KEY, { algorithm: "HS256" }, (err, decodedToken) => {
+        if (err) {
+            return res.json({ errorMessage: `User is not logged in` })
+        } else {
+            req.decodedToken = decodedToken
+            next()
+        }
+    })
+}
+
+const checkAdminAccess = (req, res, next) => {
+    if (req.decodedToken.accessLevel >= parseInt(process.env.ACCESS_LEVEL_ADMIN)) {
+        next()
+    } else {
+        res.json({ errorMessage: `User is not an administrator` })
+    }
+}
 
 // read all records
-router.get(`/products`, (req, res) =>
-{
+const getAllProducts = (req, res) => {
     //user does not have to be logged in to see product details
-    productsModel.find((error, data) =>
-    {
+    productsModel.find((error, data) => {
         res.json(data)
     })
-})
+}
 
 
 // Read one record
-router.get(`/products/:id`, (req, res) =>
-{
-    jwt.verify(req.headers.authorization, JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) =>
-    {
-        if (err)
-        {
-            res.json({errorMessage:`User is not logged in`})
-        }
-        else
-        {
-            productsModel.findById(req.params.id, (error, data) =>
-            {
-                res.json(data)
-            })
-        }
+const getProduct = (req, res) => {
+    productsModel.findById(req.params.id, (error, data) => {
+        res.json(data)
     })
-})
-
+}
 
 // Add new record
-router.post(`/products`, (req, res) =>
-{
-    jwt.verify(req.headers.authorization, JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) =>
-    {
-        if (err)
-        {
-            res.json({errorMessage:`User is not logged in`})
-        }
-        else
-        {
-            if(decodedToken.accessLevel >= process.env.ACCESS_LEVEL_ADMIN)
-            {
-                // Use the new product details to create a new product document
-                productsModel.create(req.body, (error, data) =>
-                {
-                    res.json(data)
-                })
-            }
-            else
-            {
-                res.json({errorMessage:`User is not an administrator, so they cannot add new records`})
-            }
-        }
+const createProduct = (req, res) => {
+    // Use the new product details to create a new product document
+    productsModel.create(req.body, (error, data) => {
+        res.json(data)
     })
-})
+}
 
 
 // Update one record
-router.put(`/products/:id`, (req, res) =>
-{
-    jwt.verify(req.headers.authorization, JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) =>
-    {
-        if (err)
-        {
-            res.json({errorMessage:`User is not logged in`})
-        }
-        else
-        {
-            productsModel.findByIdAndUpdate(req.params.id, {$set: req.body}, (error, data) =>
-            {
-                res.json(data)
-            })
-        }
+const updateProduct = (req, res) => {
+    // Use the new product details to update an existing product document
+    productsModel.findByIdAndUpdate(req.params.id, { $set: req.body }, (error, data) => {
+        res.json(data)
     })
-})
-
+}
 
 // Delete one record
-router.delete(`/products/:id`, (req, res) =>
-{
-
-    jwt.verify(req.headers.authorization, JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) =>
-    {
-        if (err)
-        {
-            res.json({errorMessage:`User is not logged in`})
-        }
-        else
-        {
-            if(decodedToken.accessLevel >= process.env.ACCESS_LEVEL_ADMIN)
-            {
-                productsModel.findByIdAndRemove(req.params.id, (error, data) =>
-                {
-                    res.json(data)
-                })
-            }
-            else
-            {
-                res.json({errorMessage:`User is not an administrator, so they cannot delete records`})
-            }
-        }
+const deleteProduct = (req, res) => {
+    productsModel.findByIdAndRemove(req.params.id, (error, data) => {
+        res.json(data)
     })
-})
+}
+
+router.post(`/products`, verifyUsersJWTPassword, createProduct)
+router.get(`/products`, getAllProducts)
+router.get(`/products/:id`, verifyUsersJWTPassword, getProduct)
+router.put(`/products/:id`, verifyUsersJWTPassword, updateProduct)
+router.delete(`/products/:id`, verifyUsersJWTPassword, checkAdminAccess, deleteProduct)
 
 module.exports = router
